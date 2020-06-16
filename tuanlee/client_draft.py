ebcab2f1a2732d24ce5765d4a6a5f3074a6affec
Lee Tuan <lt1679848@gmail.com>
2020-06-16 11:58:28 +0700

    FTP client download

---
+++ tuanlee/client_draft.py
@@ -0,0 +1,38 @@
+from ftplib import FTP
+import time
+import sys
+
+toolbar_width = 40
+
+server = FTP()
+server.connect('localhost', 6969)
+if (server.login('user', 'password') == '230 Login successful.'):  #connect successfully
+    files = []
+    try:
+        files = server.nlst()
+    except FTP.error_perm as resp:
+        if str(resp) == "550 No files found":
+            print ("No files in this directory")
+        else:
+            raise
+    print('All files available:')
+    i=0
+    while i < len(files):
+        print ('Press',i, 'to download', files[i])
+        i+=1
+    choosenFileIndex = input().split()
+    print(choosenFileIndex)
+    while (choosenFileIndex > i-1) :
+        choosenFileIndex = int(input('Please input correct index\n').split()[0])
+    print('**** Start downloading ****')
+    sys.stdout.write("[%s]" % (" " * toolbar_width)) #set up progressbar
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+    for i in range(toolbar_width):
+        fhandle = open(files[choosenFileIndex], 'wb')      #set up path to download file here
+        server.retrbinary('RETR ' + files[choosenFileIndex], fhandle.write)
+        fhandle.close()
+        # update the bar
+        sys.stdout.write("=")
+        sys.stdout.flush()
+    print('\n**** Download finished ****')
